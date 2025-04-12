03f8efe2b27c46dfa6e25e55e6b035c4cbad38e8
Long <longdpg.t1.2023@gmail.com>
2025-04-12 10:16:53 +0700

    update ex 1

--- L3/ex_1/seqlock.c
+++ L3/ex_1/seqlock.c
@@ -1,38 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
 #include "seqlock.h"
 
-int pthread_seqlock_init(pthread_seqlock_t *seqlock) {
-    seqlock->sequence = 0;
-    return pthread_mutex_init(&seqlock->lock, NULL);
-}
+pthread_seqlock_t lock;
 
-int pthread_seqlock_destroy(pthread_seqlock_t *seqlock) {
-    return pthread_mutex_destroy(&seqlock->lock);
-}
+int main()
+{
+    int val = 0;
 
-int pthread_seqlock_wrlock(pthread_seqlock_t *seqlock) {
-    pthread_mutex_lock(&seqlock->lock);
-    seqlock->sequence++; // odd => writing
-    return 0;
-}
+    pthread_seqlock_init(&lock);
 
-int pthread_seqlock_wrunlock(pthread_seqlock_t *seqlock) {
-    seqlock->sequence++; // even => write done
-    pthread_mutex_unlock(&seqlock->lock);
-    return 0;
-}
+    pthread_seqlock_wrlock(&lock);
+    val++;
+    pthread_seqlock_wrunlock(&lock);
 
-int pthread_seqlock_rdlock(pthread_seqlock_t *seqlock) {
-    unsigned long seq1, seq2;
-    do {
-        seq1 = seqlock->sequence;
-        __sync_synchronize(); // memory barrier
-        seq2 = seqlock->sequence;
-    } while (seq1 != seq2 || (seq1 & 1)); // retry if writing or changed
-    return 0;
+    if (pthread_seqlock_rdlock(&lock) == 1) {
+        printf("val = %d\n", val); 
+        pthread_seqlock_rdunlock(&lock);
+    }
 }
-
-int pthread_seqlock_rdunlock(pthread_seqlock_t *seqlock) {
-    // nothing to do
-    return 0;
-}
-
--- L3/ex_1/seqlock.h
+++ L3/ex_1/seqlock.h
@@ -1,18 +1,40 @@
-#ifndef SEQLOCK_H
-#define SEQLOCK_H
-
+#ifndef PTHREAD_H
 #include <pthread.h>
+#endif
+
+#include <stdatomic.h>
 
 typedef struct pthread_seqlock {
-    volatile unsigned long sequence;
+    atomic_uint seq;
     pthread_mutex_t lock;
 } pthread_seqlock_t;
 
-int pthread_seqlock_init(pthread_seqlock_t *seqlock);
-int pthread_seqlock_destroy(pthread_seqlock_t *seqlock);
-int pthread_seqlock_rdlock(pthread_seqlock_t *seqlock);
-int pthread_seqlock_rdunlock(pthread_seqlock_t *seqlock);
-int pthread_seqlock_wrlock(pthread_seqlock_t *seqlock);
-int pthread_seqlock_wrunlock(pthread_seqlock_t *seqlock);
+static inline void pthread_seqlock_init(pthread_seqlock_t *rw) {
+    atomic_init(&rw->seq, 0);
+    pthread_mutex_init(&rw->lock, NULL);
+}
 
-#endif
+static inline void pthread_seqlock_wrlock(pthread_seqlock_t *rw) {
+    pthread_mutex_lock(&rw->lock);
+    atomic_fetch_add_explicit(&rw->seq, 1, memory_order_relaxed); // Begin write (odd)
+}
+
+static inline void pthread_seqlock_wrunlock(pthread_seqlock_t *rw) {
+    atomic_fetch_add_explicit(&rw->seq, 1, memory_order_release); // End write (even)
+    pthread_mutex_unlock(&rw->lock);
+}
+
+static inline unsigned pthread_seqlock_rdlock(pthread_seqlock_t *rw) {
+    unsigned seq1;
+    do {
+        seq1 = atomic_load_explicit(&rw->seq, memory_order_acquire);
+        if (seq1 & 1) continue; // Writer in progress
+        __sync_synchronize(); // Memory barrier
+    } while (seq1 != atomic_load_explicit(&rw->seq, memory_order_acquire));
+    return 1;
+}
+
+static inline unsigned pthread_seqlock_rdunlock(pthread_seqlock_t *rw) {
+    // No-op in seqlock, because reader does not acquire a lock
+    return 0;
+}
